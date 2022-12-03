42bbbd378a1bdd11a1518e2eaeab2d2e962ab6c4
Victor <victor.hueni@eprotonmail.com>
2022-12-03 16:55:01 +0100

    load album and video games

--- src/dao/DataLoader.java
+++ src/dao/DataLoader.java
@@ -2,9 +2,9 @@ package dao;
 
 import com.mongodb.client.MongoCollection;
 import domaine.Pricing;
+import domaine.ProductType;
 import domaine.Shipping;
 import org.bson.Document;
-import org.bson.types.ObjectId;
 
 import java.text.NumberFormat;
 import java.text.ParseException;
@@ -27,44 +27,10 @@ public class DataLoader {
     public static String[][] readAlbums() { return FileToStr.lireCsv(System.getProperty("user.dir") +DATA_FOLDER + ALBUMS_FILE); }
     public static String[][] readVideoGames() { return FileToStr.lireCsv(System.getProperty("user.dir") +DATA_FOLDER + VIDEO_GAMES_FILE); }
 
-    public static int generateRandomIntInRange(int min, int max){return (int)(Math.random()*(max-min+1)+min);}
-
-    public static Pricing generatePricingBook(){
-        return new Pricing(
-                generateRandomIntInRange(10, 60),
-                generateRandomIntInRange(1, 10),
-                Math.random()
-        );
-    }
-
-    public static Shipping generateShippingBook(){
-        return new Shipping(
-                Math.random() + 0.3,
-                191,
-                136,
-                15
-        );
-    }
-
-    public static Document buildBookDocument(Pricing p, Shipping s, String[] book) {
-        /*
-        bookID
-        title
-        authors
-        average_rating
-        isbn
-        isbn13
-        language_code
-        num_pages
-        ratings_count
-        text_reviews_count
-        publication_date
-        publisher
-        */
+    private static Document buildBookDocument(Pricing p, Shipping s, String[] book) {
         Document d = new Document()
                 .append("type", "book")
                 .append("title", book[1])
-                .append("description", "")
                 .append("shipping", new Document()
                         .append("weight",s.getWeight())
                         .append("width",s.getWidth())
@@ -88,54 +54,10 @@ public class DataLoader {
         return d;
     }
 
-    public static Pricing generatePricingMovie(){
-        return new Pricing(
-                generateRandomIntInRange(10, 30),
-                generateRandomIntInRange(1, 10),
-                Math.random()
-        );
-    }
-
-    public static Shipping generateShippingMovie(){
-        return new Shipping(
-                0.105,
-                191,
-                136,
-                15
-        );
-    }
-
-    public static Date parseStringForDate(String value) {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy", Locale.FRANCE);
-        Date d = null;
-        try{
-            d = formatter.parse(value);
-        }catch(ParseException pe){
-            pe.printStackTrace();
-        }finally {
-            return d;
-        }
-    }
-
-    public static double parseDoubleStringWithComma(String value){
-        NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
-        double d = 0.0;
-        try{
-            Number number = format.parse(value);
-            d = number.doubleValue();
-        }catch(ParseException pe){
-            pe.printStackTrace();
-            d = -1.0;
-        }finally {
-            return d;
-        }
-    }
-
-    public static Document buildMovieDocument(Pricing p, Shipping s, String[] movie) {
+    private static Document buildMovieDocument(Pricing p, Shipping s, String[] movie) {
         Document d = new Document()
                 .append("type", "movie")
                 .append("title", movie[1])
-                .append("description", movie[7])
                 .append("shipping", new Document()
                                 .append("weight",s.getWeight())
                                 .append("width",s.getWidth())
@@ -165,6 +87,69 @@ public class DataLoader {
         return d;
     }
 
+    private static Document buildAlbumDocument(Pricing p, Shipping s, String[] album) {
+        Document d = new Document()
+                .append("type", "album")
+                .append("title", album[1])
+                .append("shipping", new Document()
+                        .append("weight",s.getWeight())
+                        .append("width",s.getWidth())
+                        .append("heigth",s.getHeigth())
+                        .append("depth",s.getDepth()))
+                .append("pricing", new Document()
+                        .append("selling_price",p.getSellingPrice())
+                        .append("buying_price",p.getBuyingPrice())
+                        .append("discount",p.getDiscount()))
+                .append("rolling_stone_ranking", Integer.parseInt(album[0]))
+                .append("artist", album[2])
+                .append("release_date", parseStringForDate(album[3]))
+                .append("genres", Arrays.asList(album[4].split(",")));
+
+                if(album[5] != "none"){
+                    d.append("descriptors", Arrays.asList(album[5].split(",")));
+                }
+                d.append("avg_ratings",parseDoubleStringWithComma(album[6]))
+                .append("number_of_rating", Integer.parseInt(album[7]))
+                .append("number_of_review", Integer.parseInt(album[8]));
+        return d;
+    }
+
+    private static Document buildVideoGameDocument(Pricing p, Shipping s, String[] videoGame) {
+
+        /*Rank
+        Name
+        Platform Year
+        Genre Publisher
+        NorthAmerica_Sales
+        EurpeanUnion_Sales
+        Japan_Sales
+        Other_Sales
+        Global_Sales*/
+        Document d = new Document()
+                .append("type", "video_game")
+                .append("title", videoGame[1])
+                .append("shipping", new Document()
+                        .append("weight",s.getWeight())
+                        .append("width",s.getWidth())
+                        .append("heigth",s.getHeigth())
+                        .append("depth",s.getDepth()))
+                .append("pricing", new Document()
+                        .append("selling_price",p.getSellingPrice())
+                        .append("buying_price",p.getBuyingPrice())
+                        .append("discount",p.getDiscount()))
+                .append("platform", videoGame[2])
+                .append("release_year", Integer.parseInt(videoGame[3]))
+                .append("genre", videoGame[4])
+                .append("publisher", videoGame[5])
+                .append("sales", new Document()
+                        .append("noam", parseDoubleStringWithComma(videoGame[6]) * Math.pow(10.0, 6.0))
+                        .append("eu", parseDoubleStringWithComma(videoGame[7]) * Math.pow(10.0, 6.0))
+                        .append("jpn", parseDoubleStringWithComma(videoGame[8]) * Math.pow(10.0, 6.0))
+                        .append("oth", parseDoubleStringWithComma(videoGame[9]) * Math.pow(10.0, 6.0))
+                        .append("total", parseDoubleStringWithComma(videoGame[10])* Math.pow(10.0, 6.0))
+                );
+        return d;
+    }
     public static void loadMovies() {
 
         bdd = Bdd.getInstance();
@@ -172,7 +157,11 @@ public class DataLoader {
 
         String[][] moviesFromCsv = readMovies();
         for (int i = 1; i < moviesFromCsv.length; i++) {
-            Document d = buildMovieDocument(generatePricingMovie(), generateShippingMovie(), moviesFromCsv[i]);
+            Document d = buildMovieDocument(
+                    generatePricing(ProductType.Movie),
+                    generateShipping(ProductType.Movie),
+                    moviesFromCsv[i]
+            );
             products.insertOne(d);
         }
 
@@ -185,16 +174,137 @@ public class DataLoader {
 
         String[][] booksFromCsv = readBooks();
         for (int i = 1; i < booksFromCsv.length; i++) {
-            Document d = buildBookDocument(generatePricingBook(), generateShippingBook(), booksFromCsv[i]);
+            Document d = buildBookDocument(
+                    generatePricing(ProductType.Book),
+                    generateShipping(ProductType.Book),
+                    booksFromCsv[i]
+            );
             products.insertOne(d);
         }
     }
 
     public static void loadAlbums() {
+        bdd = Bdd.getInstance();
+        MongoCollection products = bdd.getCollection("products");
+
         String[][] albumsFromCsv = readAlbums();
+        for (int i = 1; i < albumsFromCsv.length; i++) {
+            Document d = buildAlbumDocument(
+                    generatePricing(ProductType.Album),
+                    generateShipping(ProductType.Album),
+                    albumsFromCsv[i]
+            );
+            products.insertOne(d);
+        }
     }
 
     public static void loadVideoGames() {
+        bdd = Bdd.getInstance();
+        MongoCollection products = bdd.getCollection("products");
+
         String[][] videoGamesFromCsv = readVideoGames();
+        for (int i = 1; i < videoGamesFromCsv.length; i++) {
+            Document d = buildVideoGameDocument(
+                    generatePricing(ProductType.VideoGame),
+                    generateShipping(ProductType.VideoGame),
+                    videoGamesFromCsv[i]
+            );
+            products.insertOne(d);
+        }
     }
+
+    public static int generateRandomIntInRange(int min, int max){return (int)(Math.random()*(max-min+1)+min);}
+
+    public static Pricing generatePricing(ProductType type){
+        Pricing p = null;
+        switch(type) {
+            case Movie:
+                p = new Pricing(
+                        generateRandomIntInRange(10, 30),
+                        generateRandomIntInRange(1, 10),
+                        Math.random()
+                );
+                break;
+            case Book:
+                p = new Pricing(
+                        generateRandomIntInRange(10, 60),
+                        generateRandomIntInRange(1, 10),
+                        Math.random()
+                );
+                break;
+            case VideoGame:
+                p = new Pricing(
+                        generateRandomIntInRange(30, 100),
+                        generateRandomIntInRange(5, 20),
+                        Math.random()
+                );
+                break;
+            case Album:
+                p = new Pricing(
+                        generateRandomIntInRange(5, 15),
+                        generateRandomIntInRange(1, 5),
+                        Math.random()
+                );
+                break;
+        }
+        return p;
+    }
+
+    public static Shipping generateShipping(ProductType type){
+        Shipping s = null;
+        switch(type) {
+            case Movie: case VideoGame:
+                s = new Shipping(
+                        0.105,
+                        191,
+                        136,
+                        15
+                );
+                break;
+            case Book:
+                s = new Shipping(
+                        Math.random() + 0.3,
+                        191,
+                        136,
+                        15
+                );
+                break;
+            case Album:
+                s = new Shipping(
+                        20,
+                        120,
+                        120,
+                        15
+                );
+                break;
+        }
+        return s;
+    }
+
+    public static Date parseStringForDate(String value) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy", Locale.FRANCE);
+        Date d = null;
+        try{
+            d = formatter.parse(value);
+        }catch(ParseException pe){
+            pe.printStackTrace();
+        }finally {
+            return d;
+        }
+    }
+
+    public static double parseDoubleStringWithComma(String value){
+        NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
+        double d = 0.0;
+        try{
+            Number number = format.parse(value);
+            d = number.doubleValue();
+        }catch(ParseException pe){
+            pe.printStackTrace();
+            d = -1.0;
+        }finally {
+            return d;
+        }
+    }
+
 }
